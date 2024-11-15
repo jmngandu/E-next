f69622b41f422047ab84223638da5db4f97b582f
jmngandu <jamesmngandu@gmail.com>
2024-11-15 18:16:52 +0300

    e next

--- e-next/src/app/products/page.tsx
+++ e-next/src/app/products/page.tsx
@@ -7,11 +7,21 @@ export default function Products() {
 
       <h1>My products</h1>
       <ul>
-        <li>product: 1</li>
-        <li>product: 2 </li>
-        <li>product: 3</li>
-        <li>product: 4</li>
-        <li>product: 5</li>
+        <li>
+          <Link href="products/1">product: 1 </Link>
+        </li>
+        <li>
+          <Link href="products/2">product: 2</Link>
+        </li>
+        <li>
+          <Link href="products/3">product: 3</Link>
+        </li>
+        <li>
+          <Link href="products/4">product: 4</Link>
+        </li>
+        <li>
+          <Link href="products/5">product: 5</Link>
+        </li>
       </ul>
     </>
   );
